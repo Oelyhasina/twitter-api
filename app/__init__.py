e7017a337b1174a92b65af261426bbd2955855fe
Oelyhasina <hasina.oely@gmail.com>
2022-01-19 16:01:09 +0300

    Nouveau projet Flask

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+# app/__init.py__
+# pylint: disable=missing-docstring
+
+from flask import Flask
+from flask_restx import Api
+
+def create_app():
+    app = Flask(__name__)
+
+    from .apis.tweets import api as tweets
+    api = Api()
+    api.add_namespace(tweets)
+    api.init_app(app)
+
+    app.config['ERROR_404_HELP'] = False
+    return app
+    
+        
+
+    @app.route('/hello')
+    def hello():
+        return "Hello World!"
+
+    return app
